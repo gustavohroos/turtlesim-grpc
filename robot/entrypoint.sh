ad8ca52138a34e7ce7c3b457de9ebbbcd08bd53d
Gustavo Roos <gustavohroos@gmail.com>
2025-02-24 22:22:14 -0300

    feat: update Dockerfile and entrypoint

--- robot/entrypoint.sh
+++ robot/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Inicia o turtlesim em background
+source /opt/ros/humble/setup.bash
 ros2 run turtlesim turtlesim_node &
-# Aguarda alguns segundos para garantir que o turtlesim inicie e publique seus serviços
 sleep 5
-# Executa o servidor gRPC que integra o ROS2
 python3 robot_server.py
